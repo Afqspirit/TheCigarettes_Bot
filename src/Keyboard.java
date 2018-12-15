37c86d78f558dd4b95e7c7948bfd226c2c805b50
Aleksandr Zhuk <sanek.zhuk38@gmail.com>
2018-12-15 12:41:52 +0300

    create method for work with excel db ADD lib POI-OOXML 4.0

--- src/Keyboard.java
+++ src/Keyboard.java
@@ -3,11 +3,11 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
  /**                    Возомжно сюда перенесу клавиатуру
   * Todo: Подумай об этом, и сделай, НЕОБЯЗАТЕЛЬНО*/
-public class Keyboard extends TelegramLongPollingBot{
+/*public class Keyboard extends TelegramLongPollingBot{
 
 
 /**Получение id пользователя для дальнейшей работы с этим id*/
-
+/*
      public String username (){
          getMe(username);
          System.out.print(username); ;
@@ -17,7 +17,7 @@ public class Keyboard extends TelegramLongPollingBot{
 
 
 }
-
+*/
 
 
 
